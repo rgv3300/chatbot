9a0415c4a5e4d9a41fc1f6b9dfb78d2db125a77d
rgv3300 <rgv3300@gmail.com>
2021-10-15 14:17:42 -0400

    graph api security implementation

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
 require('dotenv').config()
 
-const 
-    express = require('express'),
+const express = require('express'),
     bodyParser = require('body-parser'),
-    app = express()
-    fs = require('fs')
+    got = require('got'),
+    app = express(),
+    crypto = require('crypto')
+    dayjs = require('dayjs')
+
 const port = process.env.PORT || 8001
 
+//APP secrets
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN,
+    APP_ID = process.env.APP_ID,
+    APP_SECRET = process.env.APP_SECRET,
+    VERIFY_TOKEN = process.env.VERIFY_TOKEN,
+    DATABASE_URL = process.env.DATABASE_URL
+
+// creating app secret proof and time as required by graph api
+const appsecret_time = dayjs().unix()
+const appsecret_proof = crypto.createHmac('sha256',APP_SECRET).update(ACCESS_TOKEN+ '|' + appsecret_time).digest('hex') 
+
+
+const graphapi = "https://graph.facebook.com" 
 
 app.use(bodyParser.json())   //body parser middleware
 
-//webhook endpoing
+//webhook endpoint event listener 
 app.post('/webhook', (req,res) => {
-    let body = req.body
+    
 
-    console.log(body)
     res.status(200).send('Event received')
 })
 
 //token verification
 app.get('/webhook', (req,res) => {
-    const VERIFY_TOKEN = 'gnYTdLeuRP'
-
+    
     const token = req.query['hub.verify_token']
     const challenge = req.query['hub.challenge']
 
     if(token) {
         if(token == VERIFY_TOKEN) {
-            console.log('verified')
             res.status(200).send(challenge)
         } else {
             res.sendStatus(403)
@@ -36,10 +48,8 @@ app.get('/webhook', (req,res) => {
 })
 
 
-app.get('/',(req,res) => {
-    res.send('hello world')
-})
 
-app.listen(port, () => {
-    console.log('Hello world!')
-})
+
+// app.listen(port, () => {
+//     console.log('Hello world!')
+// })
