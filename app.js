889c20c44a4ec7a873264446291ba14e25617fcb
rgv3300 <rgv3300@gmail.com>
2021-10-20 11:43:54 -0400

    created in memory mock DB for demo

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { send } = require('process')
+
 require('dotenv').config()
 
 
@@ -17,18 +19,26 @@ const port = process.env.PORT || 8000
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN,
     APP_ID = process.env.APP_ID,
     APP_SECRET = process.env.APP_SECRET,
-    VERIFY_TOKEN = process.env.VERIFY_TOKEN,
-    DATABASE_URL = process.env.DATABASE_URL
-
-//creating app secret proof and time as required by graph api
-const appsecret_time = dayjs().unix()
-const appsecret_proof = crypto.createHmac('sha256',APP_SECRET).update(ACCESS_TOKEN+ '|' + appsecret_time).digest('hex') 
-
-console.log(appsecret_proof + ' ' + appsecret_time)
+    VERIFY_TOKEN = process.env.VERIFY_TOKEN
+ 
 // acceptable greetings
 const acceptGreet = ['Hello','Hi','hello','hi','hey','Hey','getstarted']
 
 const expenses = ['supplies','salaries','legal','insurance','tax','maintenance','advertisement']
+
+//state variables
+let chatState = [] 
+let expenseSelected;
+const dbMock = new Map()
+const expense = new Map()
+expense.set("supplies",[])
+expense.set("salaries",[])
+expense.set("legal",[])
+expense.set("insurance",[])
+expense.set("tax",[])
+expense.set("maintenance",[])
+expense.set("advertisement",[])
+//
 
 let graphapi = "https://graph.facebook.com/v12.0"
 
@@ -42,7 +52,7 @@ function setGetStarted() {
 
     let conf = {
         method:'post',
-        url: `${graphapi}/me/messenger_profile?access_token=${ACCESS_TOKEN}&appsecret_proof=${appsecret_proof}&appsecret_time=${appsecret_time}`,
+        url: `${graphapi}/me/messenger_profile?access_token=${ACCESS_TOKEN}`,
         headers: { 
             'Content-Type': 'application/json'
           },
@@ -58,29 +68,37 @@ function setGetStarted() {
 
 }
 
-
-
 //handlers
 function messageHandler(sender_psid, received_message) {
 
     let response;
 
-    if(!(acceptGreet.includes(received_message))) {
+    if(!(acceptGreet.includes(received_message)) && !chatState[sender_psid]) {
 
         response = {
             "text" : `Sorry, I am still learning and cannot process your request for "${received_message}". Please choose one of the options to proceed.`
         }
         
+        sendAPI(sender_psid, response)
 
-    } else {
+        sendAPI(sender_psid, menu)
+
+
+    } else if(acceptGreet.includes(received_message) && !chatState[sender_psid]){
 
         response = { "text": `Howdy! I will help you manage your office expenses and generate useful reports for you! Type "Hey" anytime to start over` }
     
+        sendAPI(sender_psid, response)    
+
+        sendAPI(sender_psid, menu)
+
+    } else {
+
+        response = {'text': `Please enter the amount for the previous expense selected`}
+
+        sendAPI(sender_psid, response)
+
     }
-
-    sendAPI(sender_psid, response)
-
-    sendAPI(sender_psid, menu)
 }
 
 function postbackHandler(sender_psid,received_postback) {
@@ -93,34 +111,61 @@ function postbackHandler(sender_psid,received_postback) {
         
         response = expenseMenu
         
+        sendAPI(sender_psid,response)
 
     } else if(payload === 'report') {
+        //calculate expense
+        response = {"text": `Your total expense is {{expense}}.`}
         
-        response = {"text": `Your total expense is {{expense}}. You spent the most on {{category}}`}
-        
-    }
+        sendAPI(sender_psid,response)
 
-    if(payload === 'report') {
+        sendAPI(sender_psid,menu)
+    }
         
-        const response2 = {"text": `To get categorize list of expenses type "expense by category" or type "Hey" to start over.`}
-        
-        sendAPI(sender_psid,response2)
-   
-    } 
-        
-    sendAPI(sender_psid,response)
+    
 
 }
 
 function quickReplyHandler(sender_psid,received_message) {
 
     if(expenses.includes(received_message)) {
-        
-        let response = {"text" : `Please enter the amount for "${received_message}" category`}
+
+        chatState[sender_psid] = 1
+
+        expenseSelected = received_message
+
+        console.log(chatState[sender_psid] + ' ' + expenseSelected)
+
+        let response = {"text" : `Please enter the amount for "${received_message}" category. Amount should only be numeric`}
 
         sendAPI(sender_psid, response)
+
+        //if amount added respond with success msg and menu
     }
 
+}
+
+function expenseHandler(sender_psid, amount) {
+
+    const id = parseInt(sender_psid)
+
+    if(!dbMock.has(id)) {
+
+        dbMock.set(id,expense)
+
+    }
+
+    dbMock.get(id).get(expenseSelected).push(parseInt(amount))
+
+    let response = {"text": `Your expense has been successfully added to "${expenseSelected}" category.`}
+    
+    sendAPI(sender_psid,response)
+
+    sendAPI(sender_psid,menu)
+
+    //reset state after adding expenses
+    chatState[sender_psid] = 0
+    expenseSelected = ''
 }
 
 // send api to reply to messages
@@ -137,7 +182,7 @@ function sendAPI(sender_psid, response) {
 
     let config = {
         method:'post',
-        url: `${graphapi}/me/messages?access_token=${ACCESS_TOKEN}&appsecret_proof=${appsecret_proof}&appsecret_time=${appsecret_time}`,
+        url: `${graphapi}/me/messages?access_token=${ACCESS_TOKEN}`,
         headers: {
             'Content-Type': 'application/json'
         },
@@ -146,7 +191,7 @@ function sendAPI(sender_psid, response) {
    
     axios(config)
         .then((res) => {
-            
+            console.log(res.body)
         },(error) => {
             console.log(error)
         })
@@ -173,6 +218,10 @@ app.post('/webhook', (req,res) => {
                 if(webhook_event.message.quick_reply) {
 
                     quickReplyHandler(sender_psid, webhook_event.message.quick_reply.payload)
+
+                } else if (chatState[sender_psid] && !isNaN(webhook_event.message.text)) {
+                    
+                    expenseHandler(sender_psid, webhook_event.message.text)
 
                 } else {
 
